b90544379f8f7aa13bc63a3863d27b2961812ac4
kavitshah8 <kavit.shah8@gmail.com>
2014-03-09 12:14:47 -0400

    removes predefined files with exprespress
    handling GET and returning JSON

--- nodeJS/airline/app.js
+++ nodeJS/airline/app.js
@@ -1,14 +1,9 @@
-
-/**
- * Module dependencies.
- */
-
+// Module dependencies.
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
+//var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-
 var app = express();
 
 // all environments
@@ -28,8 +23,9 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/flight1', routes.flight1);
+app.get('/flight2', routes.flight2);
+//app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- nodeJS/airline/routes/index.js
+++ nodeJS/airline/routes/index.js
@@ -1,8 +1,24 @@
+// GET home page
+var flight = require('../flight');
+var flight1 = flight({
+	number: 1,
+	origin: 'LAX',
+	destination: 'DCA',
+	departs: '9AM',
+	arrives: '4PM'
+});
+var flight2 = flight({
+	number: 2,
+	origin: 'LAX',
+	destination: 'PDX',
+	departs: '10AM',
+	arrives: '12PM'
+});
 
-/*
- * GET home page.
- */
+module.exports.flight1 = function(req, res){
+  res.json(flight1.getInformation());
+};
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+module.exports.flight2 = function(req, res){
+  res.json(flight2.getInformation());
 };
