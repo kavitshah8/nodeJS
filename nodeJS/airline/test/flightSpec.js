5ee32acd872ebab96bcbf6f6b722aa37a9652037
kavitshah8 <kavit.shah8@gmail.com>
2014-03-09 23:22:45 -0400

    unit test with Mocha, supertest, should

--- nodeJS/airline/test/flightSpec.js
+++ nodeJS/airline/test/flightSpec.js
@@ -6,11 +6,22 @@ var should = require('should'),
 describe('flights',function () {
 	
 	it('it should pass', function (done) {
-		done();
+		supertest(app)
+		.get('/flight/18')
+		.expect(200)
+		.end(function (err, res) {
+			res.status.should.equal(200);
+			done();
+		});
 	});
 
 	it('it should not pass', function (done) {
-		//throw "error";
-		done();
+		supertest(app)
+		.get('/flight/9999999')
+		.expect(404)
+		.end(function (err, res){
+			res.status.should.equal(404);
+			done();
+		})
 	});
 });
