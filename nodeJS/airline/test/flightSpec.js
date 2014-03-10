98e25528535983885b627f65f687a27bb194f441
kavitshah8 <kavit.shah8@gmail.com>
2014-03-09 20:20:30 -0400

    adds two unit tests using mocha

---
+++ nodeJS/airline/test/flightSpec.js
@@ -0,0 +1,16 @@
+var app = require('./helper/app');
+
+var should = require('should'),
+	supertest = require('supertest');
+
+describe('flights',function () {
+	
+	it('it should pass', function (done) {
+		done();
+	});
+
+	it('it should not pass', function (done) {
+		//throw "error";
+		done();
+	});
+});
